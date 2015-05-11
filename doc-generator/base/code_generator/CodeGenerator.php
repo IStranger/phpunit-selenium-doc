9e89bf95137b4873536e2c0f721b83add7a76ec1
IStranger <bgu@bk.ru>
2015-05-11 18:55:34 +0600

    extended code generator   - added generation arguments and returnValue for DocBlock of method

--- doc-generator/base/code_generator/CodeGenerator.php
+++ doc-generator/base/code_generator/CodeGenerator.php
@@ -15,9 +15,9 @@ class CodeGenerator
     const TPL_CLASS  = 'tpl/class.tpl';
     const TPL_METHOD = 'tpl/method.tpl';
 
-    /**
-     * Useful width of DocBlock of method (symbol count)
-     */
+    /** Prefix for each line of DocBlock of method */
+    const METHOD_DOC_BLOCK_PREFIX = ' * ';
+    /** Useful width of DocBlock of method (symbol count) */
     const DOC_BLOCK_WIDTH = 112;
 
     function __construct()
@@ -46,9 +46,9 @@ class CodeGenerator
                 '%method.name%'                      => $method->name,
                 '%method.arguments:list%'            => $this->getArgumentListAsPhp($method),
                 '%method.description:php_doc%'       => $this->getDescriptionAsPhpDoc($method),
-                '%method.arguments:php_doc%'         => '*',
-                '%method.returnValue:php_doc%'       => '*',
-                '%method.derivativeMethods:php_doc%' => '*',
+                '%method.arguments:php_doc%'         => $this->getArgumentsAsPhpDoc($method),
+                '%method.returnValue:php_doc%'       => $this->getReturnValueAsPhpDoc($method),
+                '%method.derivativeMethods:php_doc%' => self::METHOD_DOC_BLOCK_PREFIX,
             ]);
         }
         $code = strtr($this->_tplClass, [
@@ -75,14 +75,70 @@ class CodeGenerator
         return join(', ', $argumentList);
     }
 
+    /**
+     * Returns arguments description for DocBlock of specified method
+     *
+     * @param Method $method
+     *
+     * @return string           Parts of DocBlock of specified method
+     */
+    protected function getArgumentsAsPhpDoc(Method $method)
+    {
+        $phpDoc = [];
+
+        // type + name of argument
+        $maxLength = 0;
+        foreach ($method->arguments as $argument) {
+            $phpDoc[$argument->name] = '@param ' . $argument->type . '   $' . $argument->name . '  ';
+            $length = strlen($phpDoc[$argument->name]);
+            $maxLength = ($length > $maxLength) ? $length : $maxLength;
+        }
+
+        // add formatted description for arguments (aligned for all arguments)
+        $descriptionLength = self::DOC_BLOCK_WIDTH - $maxLength;
+        $firstSpaces = str_repeat(' ', $maxLength);
+        foreach ($method->arguments as $argument) {
+            $argDescription = wordwrap($argument->description, $descriptionLength, PHP_EOL . $firstSpaces);
+            $phpDoc[$argument->name] = str_pad($phpDoc[$argument->name], $maxLength) . $argDescription . PHP_EOL;
+        }
+
+        return $this->addInLineBeginning(join('', $phpDoc));
+    }
+
+    /**
+     * Returns description of specified method for DocBlock
+     *
+     * @param Method $method
+     *
+     * @return string       Parts of DocBlock of specified method
+     */
     protected function getDescriptionAsPhpDoc(Method $method)
     {
         // need ignore of <code> blocks ...
-        return $this->addFirstStar(wordwrap($method->description, self::DOC_BLOCK_WIDTH, PHP_EOL));
+        return $this->addInLineBeginning(wordwrap($method->description, self::DOC_BLOCK_WIDTH, PHP_EOL));
     }
 
-    protected function addFirstStar($str)
+    protected function getReturnValueAsPhpDoc(Method $method)
     {
-        return '* ' . str_replace(PHP_EOL, PHP_EOL . '* ', $str);
+        $phpDoc = '@return  ' . $method->returnValue->type . '  ';
+        $length = strlen($phpDoc);
+        $descriptionLength = self::DOC_BLOCK_WIDTH - $length;
+        $firstSpaces = str_repeat(' ', $length);
+        $phpDoc = $phpDoc . wordwrap($method->returnValue->description, $descriptionLength, PHP_EOL . $firstSpaces);
+
+        return $this->addInLineBeginning($phpDoc);
+    }
+
+    /**
+     * Adds specified string in beginning of each line of specified text.
+     *
+     * @param string $multiLineText Multi line text
+     * @param string $addString     String to add
+     *
+     * @return string
+     */
+    protected function addInLineBeginning($multiLineText, $addString = self::METHOD_DOC_BLOCK_PREFIX)
+    {
+        return $addString . str_replace(PHP_EOL, PHP_EOL . $addString, $multiLineText);
     }
 }
--- doc-generator/base/code_generator/tpl/method.tpl
+++ doc-generator/base/code_generator/tpl/method.tpl
@@ -1,10 +1,10 @@
 /**
- %method.description:php_doc%
+%method.description:php_doc%
  *
- %method.arguments:php_doc%
+%method.arguments:php_doc%
  *
- %method.returnValue:php_doc%
- %method.derivativeMethods:php_doc%
+%method.returnValue:php_doc%
+%method.derivativeMethods:php_doc%
  */
 public function %method.name%(%method.arguments:list%)
 {
